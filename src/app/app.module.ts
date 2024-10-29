40b1957bf34ed2e84c2276357e6cabb569baa647
JassCast18 <125106510+JassCast18@users.noreply.github.com>
2024-10-29 10:39:10 -0600

    Actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { AboutComponent } from './components/about/about.component';
+import { ItemComponent } from './components/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PrincipalComponent,
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
